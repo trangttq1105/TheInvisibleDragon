42fefbfd5bf19c1726404a378a41b2414c89edb3
trangttq1105 <trangttq1105@gmail.com>
2026-08-27 16:37:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const CONFIG = {
     // Thailand time / GMT+7
     // ========================================================
 
-    trailerTime: "2026-08-27T16:30:00+07:00",
+    trailerTime: "2026-08-27T16:42:00+07:00",
 
 
     // ========================================================
@@ -18,7 +18,7 @@ const CONFIG = {
     // Thailand time / GMT+7
     // ========================================================
 
-    specialEPTime: "2026-08-29T16:30:00+07:00",
+    specialEPTime: "2026-08-29T16:42:00+07:00",
 
 
     // ========================================================
@@ -478,15 +478,6 @@ function renderTrailerActions() {
     // STREAMING
     // -----------------------------
 
-    streamingActions.appendChild(
-
-        createActionButton(
-            "STREAMING",
-            LINKS.howToStream
-        )
-
-    );
-
 
     streamingActions.appendChild(
 
@@ -546,16 +537,6 @@ function renderTrailerActions() {
     // -----------------------------
     // TRENDING
     // -----------------------------
-
-    trendingActions.appendChild(
-
-        createActionButton(
-            "TRENDING",
-            LINKS.howToTrend
-        )
-
-    );
-
 
     trendingActions.appendChild(
 
